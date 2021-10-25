6f64ba33a3377351fa790943ae6392ef29da644f
Philipp van Kempen <philipp.van-kempen@tum.de>
2021-10-25 07:11:49 +0200

    add files

---
+++ apps/microtvm/etissvp/template_project/microtvm_api_server.py
@@ -0,0 +1,432 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import atexit
+import collections
+import collections.abc
+import enum
+import fcntl
+import logging
+import os
+import os.path
+import pathlib
+import queue
+import re
+import select
+import shlex
+import shutil
+import subprocess
+import sys
+import tarfile
+import tempfile
+import threading
+import time
+import json
+
+import serial
+import serial.tools.list_ports
+import yaml
+
+from tvm.micro.project_api import server
+
+
+_LOG = logging.getLogger(__name__)
+
+
+API_SERVER_DIR = pathlib.Path(os.path.dirname(__file__) or os.path.getcwd())
+
+
+BUILD_DIR = API_SERVER_DIR / "build"
+
+
+MODEL_LIBRARY_FORMAT_RELPATH = "model.tar"
+
+
+IS_TEMPLATE = not (API_SERVER_DIR / MODEL_LIBRARY_FORMAT_RELPATH).exists()
+
+def check_call(cmd_args, *args, **kwargs):
+    cwd_str = "" if "cwd" not in kwargs else f" (in cwd: {kwargs['cwd']})"
+    _LOG.info("run%s: %s", cwd_str, " ".join(shlex.quote(a) for a in cmd_args))
+    return subprocess.check_call(cmd_args, *args, **kwargs)
+
+
+CACHE_ENTRY_RE = re.compile(r"(?P<name>[^:]+):(?P<type>[^=]+)=(?P<value>.*)")
+
+
+CMAKE_BOOL_MAP = dict(
+    [(k, True) for k in ("1", "ON", "YES", "TRUE", "Y")]
+    + [(k, False) for k in ("0", "OFF", "NO", "FALSE", "N", "IGNORE", "NOTFOUND", "")]
+)
+
+
+class CMakeCache(collections.abc.Mapping):
+    def __init__(self, path):
+        self._path = path
+        self._dict = None
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __getitem__(self, key):
+        if self._dict is None:
+            self._dict = self._read_cmake_cache()
+
+        return self._dict[key]
+
+    def __len__(self):
+        return len(self._dict)
+
+    def _read_cmake_cache(self):
+        """Read a CMakeCache.txt-like file and return a dictionary of values."""
+        entries = collections.OrderedDict()
+        with open(self._path, encoding="utf-8") as f:
+            for line in f:
+                m = CACHE_ENTRY_RE.match(line.rstrip("\n"))
+                if not m:
+                    continue
+
+                if m.group("type") == "BOOL":
+                    value = CMAKE_BOOL_MAP[m.group("value").upper()]
+                else:
+                    value = m.group("value")
+
+                entries[m.group("name")] = value
+
+        return entries
+
+
+CMAKE_CACHE = CMakeCache(BUILD_DIR / "CMakeCache.txt")
+
+
+class BoardError(Exception):
+    """Raised when an attached board cannot be opened (i.e. missing /dev nodes, etc)."""
+
+
+PROJECT_TYPES = []
+if IS_TEMPLATE:
+    for d in (API_SERVER_DIR / "src").iterdir():
+        if d.is_dir():
+            PROJECT_TYPES.append(d.name)
+
+
+PROJECT_OPTIONS = [
+    server.ProjectOption(
+        "extra_files_tar",
+        help="If given, during generate_project, uncompress the tarball at this path into the project dir.",
+    ),
+    server.ProjectOption(
+        "etiss_path",
+        help="Path to the installed ETISS directory.",
+    ),
+    server.ProjectOption(
+        "riscv_path",
+        help="Path to the installed RISCV GCC directory.",
+    ),
+    server.ProjectOption(
+        "etissvp_script",
+        help="Path to script which sets up the environment and starts running the provided target software binary on the vp.",
+    ),
+    server.ProjectOption(
+        "project_type",
+        help="Type of project to generate.",
+        choices=tuple(PROJECT_TYPES),
+    ),
+    server.ProjectOption("verbose", help="Run build with verbose output.", choices=(True, False)),
+    server.ProjectOption("debug", help="Run build in DEBUG mode.", choices=(True, False)),
+]
+
+
+class Handler(server.ProjectAPIHandler):
+    def __init__(self):
+        super(Handler, self).__init__()
+        self._proc = None
+
+    def server_info_query(self, tvm_version):
+        return server.ServerInfo(
+            platform_name="etissvp",
+            is_template=IS_TEMPLATE,
+            model_library_format_path=""
+            if IS_TEMPLATE
+            else (API_SERVER_DIR / MODEL_LIBRARY_FORMAT_RELPATH),
+            project_options=PROJECT_OPTIONS,
+        )
+
+    # These files and directories will be recursively copied into generated projects from the CRT.
+    CRT_COPY_ITEMS = ("include", "Makefile", "src")
+
+    API_SERVER_CRT_LIBS_TOKEN = "<API_SERVER_CRT_LIBS>"
+
+    CRT_LIBS_BY_PROJECT_TYPE = {
+        "host_driven": "microtvm_rpc_server microtvm_rpc_common common",
+        "aot_demo": "memory microtvm_rpc_common common",
+    }
+
+    def generate_project(self, model_library_format_path, standalone_crt_dir, project_dir, options):
+        project_dir = pathlib.Path(project_dir)
+        # Make project directory.
+        project_dir.mkdir()
+
+        # Copy ourselves to the generated project. TVM may perform further build steps on the generated project
+        # by launching the copy.
+        shutil.copy2(__file__, project_dir / os.path.basename(__file__))
+
+        # Place Model Library Format tarball in the special location, which this script uses to decide
+        # whether it's being invoked in a template or generated project.
+        project_model_library_format_tar_path = project_dir / MODEL_LIBRARY_FORMAT_RELPATH
+        shutil.copy2(model_library_format_path, project_model_library_format_tar_path)
+
+        # Extract Model Library Format tarball.into <project_dir>/model.
+        extract_path = os.path.splitext(project_model_library_format_tar_path)[0]
+        with tarfile.TarFile(project_model_library_format_tar_path) as tf:
+            os.makedirs(extract_path)
+            tf.extractall(path=extract_path)
+
+        # Populate CRT.
+        crt_path = project_dir / "crt"
+        crt_path.mkdir()
+        for item in self.CRT_COPY_ITEMS:
+            src_path = os.path.join(standalone_crt_dir, item)
+            dst_path = crt_path / item
+            if os.path.isdir(src_path):
+                shutil.copytree(src_path, dst_path)
+            else:
+                shutil.copy2(src_path, dst_path)
+
+        # Populate Makefile.
+        with open(API_SERVER_DIR / "CMakeLists.txt.template", "r") as cmake_template_f:
+            with open(project_dir / "CMakeLists.txt", "w") as cmake_f:
+                for line in cmake_template_f:
+                    if self.API_SERVER_CRT_LIBS_TOKEN in line:
+                        crt_libs = self.CRT_LIBS_BY_PROJECT_TYPE[options["project_type"]]
+                        line = line.replace("<API_SERVER_CRT_LIBS>", crt_libs)
+
+                    cmake_f.write(line)
+
+        #self._create_prj_conf(project_dir, options)
+
+        # Populate crt-config.h
+        crt_config_dir = project_dir / "crt_config"
+        crt_config_dir.mkdir()
+        shutil.copy2(
+            API_SERVER_DIR / "crt_config" / "crt_config.h", crt_config_dir / "crt_config.h"
+        )
+
+        # Populate src/
+        src_dir = project_dir / "src"
+        shutil.copytree(API_SERVER_DIR / "src" / options["project_type"], src_dir)
+        #print(src_dir)
+        #input()
+
+        # Populate extra_files
+        if options.get("extra_files_tar"):
+            with tarfile.open(options["extra_files_tar"], mode="r:*") as tf:
+                tf.extractall(project_dir)
+
+    def build(self, options):
+        BUILD_DIR.mkdir()
+
+        cmake_args = ["cmake", ".."]
+
+        if options.get("etiss_dir"):
+            cmake_args.append("-DETISS_DIR=" + options["etiss_dir"])
+        else:
+            raise RuntimeError("Project Config 'etiss_dir' undefined!")
+
+        if options.get("riscv_dir"):
+            cmake_args.append("-DRISCV_ELF_GCC_PREFIX=" + options["riscv_dir"])
+        else:
+            raise RuntimeError("Project Config 'riscv_dir' undefined!")
+
+        if options.get("debug"):
+            cmake_args.append("-DCMAKE_BUILD_TYPE=DEBUG")
+
+        if options.get("verbose"):
+            cmake_args.append("-DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE")
+
+        print(BUILD_DIR)
+        input()
+        check_call(cmake_args, cwd=BUILD_DIR)
+
+        args = ["make", "-j2"]
+        if options.get("verbose"):
+            args.append("VERBOSE=1")
+        check_call(args, cwd=BUILD_DIR)
+
+    def flash(self, options):
+        return  # NOTE: etissvp requires no flash step--it is launched from open_transport.
+
+    def open_transport(self, options):
+        transport = ETISSVPTransport(options)
+
+        to_return = transport.open()
+        self._transport = transport
+        atexit.register(lambda: self.close_transport())
+        return to_return
+
+    def close_transport(self):
+        if self._transport is not None:
+            self._transport.close()
+            self._transport = None
+
+    def read_transport(self, n, timeout_sec):
+        if self._transport is None:
+            raise server.TransportClosedError()
+
+        return self._transport.read(n, timeout_sec)
+
+    def write_transport(self, data, timeout_sec):
+        if self._transport is None:
+            raise server.TransportClosedError()
+
+        return self._transport.write(data, timeout_sec)
+
+
+def _set_nonblock(fd):
+    flag = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+    new_flag = fcntl.fcntl(fd, fcntl.F_GETFL)
+    assert (new_flag & os.O_NONBLOCK) != 0, "Cannot set file descriptor {fd} to non-blocking"
+
+
+class ETISSVPMakeResult(enum.Enum):
+    ETISSVP_STARTED = "etissvp_started"
+    MAKE_FAILED = "make_failed"
+    EOF = "eof"
+
+
+class ETISSVPTransport:
+    """The user-facing ETISSVP transport class."""
+
+    def __init__(self, options):
+        self.options = options
+        self.proc = None
+        self.pipe_dir = None
+        self.read_fd = None
+        self.write_fd = None
+        self._queue = queue.Queue()
+
+    def open(self):
+        #self.pipe_dir = pathlib.Path(tempfile.mkdtemp())
+        self.pipe_dir = os.path.join(BUILD_DIR, ".tmp")
+        self.write_pipe = self.pipe_dir / "uartdevicefifoin"
+        self.read_pipe = self.pipe_dir / "uartdevicefifoout"
+        #os.mkfifo(self.write_pipe)
+        #os.mkfifo(self.read_pipe)
+
+        print("RUN", BUILD_DIR)
+        input()
+        if not options.get("etissvp_script"):
+            raise RuntimeError("Project Config 'etissvp_script' undefined!")
+        self.proc = subprocess.Popen(
+            [options["etissvp_script"], "app"],
+            #["make", "run", f"UART_PIPE={self.pipe}"],
+            cwd=BUILD_DIR,
+            stdout=subprocess.PIPE,
+        )
+        self._wait_for_etissvp()
+
+        # NOTE: although each pipe is unidirectional, open both as RDWR to work around a select
+        # limitation on linux. Without this, non-blocking I/O can't use timeouts because named
+        # FIFO are always considered ready to read when no one has opened them for writing.
+        self.read_fd = os.open(self.read_pipe, os.O_RDWR | os.O_NONBLOCK)
+        self.write_fd = os.open(self.write_pipe, os.O_RDWR | os.O_NONBLOCK)
+        _set_nonblock(self.read_fd)
+        _set_nonblock(self.write_fd)
+
+        return server.TransportTimeouts(
+            session_start_retry_timeout_sec=2.0,
+            session_start_timeout_sec=10.0,
+            session_established_timeout_sec=10.0,
+        )
+
+    def close(self):
+        did_write = False
+        if self.write_fd is not None:
+            try:
+                server.write_with_timeout(
+                    self.write_fd, b"\x01x", 1.0
+                )  # Use a short timeout since we will kill the process
+                did_write = True
+            except server.IoTimeoutError:
+                pass
+            os.close(self.write_fd)
+            self.write_fd = None
+
+        if self.proc:
+            if not did_write:
+                self.proc.terminate()
+            try:
+                self.proc.wait(5.0)
+            except subprocess.TimeoutExpired:
+                self.proc.kill()
+
+        if self.read_fd:
+            os.close(self.read_fd)
+            self.read_fd = None
+
+        if self.pipe_dir is not None:
+            shutil.rmtree(self.pipe_dir)
+            self.pipe_dir = None
+
+    def read(self, n, timeout_sec):
+        return server.read_with_timeout(self.read_fd, n, timeout_sec)
+
+    def write(self, data, timeout_sec):
+        to_write = bytearray()
+        escape_pos = []
+        for i, b in enumerate(data):
+            if b == 0x01:
+                to_write.append(b)
+                escape_pos.append(i)
+            to_write.append(b)
+
+        while to_write:
+            num_written = server.write_with_timeout(self.write_fd, to_write, timeout_sec)
+            to_write = to_write[num_written:]
+
+    def _etissvp_check_stdout(self):
+        for line in self.proc.stdout:
+            line = str(line)
+            _LOG.info("%s", line)
+            if "[QEMU] CPU" in line:  # TODO
+                self._queue.put(ETISSVPMakeResult.ETISSVP_STARTED)
+            else:
+                line = re.sub("[^a-zA-Z0-9 \n]", "", line)
+                pattern = r"recipe for target (\w*) failed"
+                if re.search(pattern, line, re.IGNORECASE):
+                    self._queue.put(ETISSVPMakeResult.MAKE_FAILED)
+        self._queue.put(ETISSVPMakeResult.EOF)
+
+    def _wait_for_etissvp(self):
+        threading.Thread(target=self._etissvp_check_stdout, daemon=True).start()
+        while True:
+            try:
+                item = self._queue.get(timeout=120)
+            except Exception:
+                raise TimeoutError("ETISSVP setup timeout.")
+
+            if item == ETISSVPMakeResult.ETISSVP_STARTED:
+                break
+
+            if item in [ETISSVPMakeResult.MAKE_FAILED, ETISSVPMakeResult.EOF]:
+                raise RuntimeError("ETISSVP setup failed.")
+
+            raise ValueError(f"{item} not expected.")
+
+
+if __name__ == "__main__":
+    server.main(Handler())
---
+++ apps/microtvm/etissvp/template_project/src/host_driven/main.c
@@ -0,0 +1,252 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * This is a sample Zephyr-based application that contains the logic
+ * needed to control a microTVM-based model via the UART. This is only
+ * intended to be a demonstration, since typically you will want to incorporate
+ * this logic into your own application.
+ */
+
+//#include <drivers/gpio.h>
+//#include <drivers/uart.h>
+//#include <fatal.h>
+//#include <kernel.h>
+//#include <power/reboot.h>
+//#include <random/rand32.h>
+#include <stdio.h>
+#include <stdbool.h>
+//#include <sys/printk.h>
+//#include <sys/ring_buffer.h>
+#include <tvm/runtime/crt/logging.h>
+#include <tvm/runtime/crt/microtvm_rpc_server.h>
+#include <unistd.h>
+//#include <zephyr.h>
+
+#include "crt_config.h"
+
+static const struct device* tvm_uart;
+
+static size_t g_num_bytes_requested = 0;
+static size_t g_num_bytes_written = 0;
+static size_t g_num_bytes_in_rx_buffer = 0;
+
+unsigned long micros() {
+  return 0;
+}
+
+// Called by TVM to write serial data to the UART.
+ssize_t write_serial(void* unused_context, const uint8_t* data, size_t size) {
+  g_num_bytes_requested += size;
+
+  for (size_t i = 0; i < size; i++) {
+    //uart_poll_out(tvm_uart, data[i]);
+    printf("uart_poll_out: %d (%c)", data[i], data[i]);
+    g_num_bytes_written++;
+  }
+
+  return size;
+}
+
+// Called by TVM when a message needs to be formatted.
+size_t TVMPlatformFormatMessage(char* out_buf, size_t out_buf_size_bytes, const char* fmt,
+                                va_list args) {
+  return vsnprintf(out_buf, out_buf_size_bytes, fmt, args);
+}
+
+// Called by TVM when an internal invariant is violated, and execution cannot continue.
+void TVMPlatformAbort(tvm_crt_error_t error) {
+  TVMLogf("TVMError: 0x%x", error);
+  //sys_reboot(SYS_REBOOT_COLD);
+  // TODO
+  for (;;)
+    ;
+}
+
+// Called by TVM to generate random data.
+tvm_crt_error_t TVMPlatformGenerateRandom(uint8_t* buffer, size_t num_bytes) {
+  uint32_t random;  // one unit of random data.
+
+  // Fill parts of `buffer` which are as large as `random`.
+  size_t num_full_blocks = num_bytes / sizeof(random);
+  for (int i = 0; i < num_full_blocks; ++i) {
+    //random = sys_rand32_get();
+    // TODO
+    memcpy(&buffer[i * sizeof(random)], &random, sizeof(random));
+  }
+
+  // Fill any leftover tail which is smaller than `random`.
+  size_t num_tail_bytes = num_bytes % sizeof(random);
+  if (num_tail_bytes > 0) {
+    //random = sys_rand32_get();
+    // TODO
+    memcpy(&buffer[num_bytes - num_tail_bytes], &random, num_tail_bytes);
+  }
+  return kTvmErrorNoError;
+}
+
+// Heap for use by TVMPlatformMemoryAllocate.
+//K_HEAP_DEFINE(tvm_heap, 216 * 1024);
+tvm_crt_error_t TVMPlatformMemoryAllocate(size_t num_bytes, DLDevice dev, void** out_ptr) {
+  if (num_bytes == 0) {
+    num_bytes = sizeof(int);
+  }
+  *out_ptr = malloc(num_bytes);
+  return (*out_ptr == NULL) ? kTvmErrorPlatformNoMemory : kTvmErrorNoError;
+}
+
+tvm_crt_error_t TVMPlatformMemoryFree(void* ptr, DLDevice dev) {
+  free(ptr);
+  return kTvmErrorNoError;
+}
+
+// Called by TVM to allocate memory.
+/*tvm_crt_error_t TVMPlatformMemoryAllocate(size_t num_bytes, DLDevice dev, void** out_ptr) {
+  //*out_ptr = k_heap_alloc(&tvm_heap, num_bytes, K_NO_WAIT);
+  // TODO
+  return (*out_ptr == NULL) ? kTvmErrorPlatformNoMemory : kTvmErrorNoError;
+}
+
+// Called by TVM to deallocate memory.
+tvm_crt_error_t TVMPlatformMemoryFree(void* ptr, DLDevice dev) {
+  //k_heap_free(&tvm_heap, ptr);
+  // TODO
+  return kTvmErrorNoError;
+}*/
+
+//#define MILLIS_TIL_EXPIRY 200
+//#define TIME_TIL_EXPIRY (K_MSEC(MILLIS_TIL_EXPIRY))
+//K_TIMER_DEFINE(g_microtvm_timer, /* expiry func */ NULL, /* stop func */ NULL);
+
+unsigned long g_utvm_start_time_micros;
+int g_utvm_timer_running = 0;
+
+tvm_crt_error_t TVMPlatformTimerStart() {
+  if (g_utvm_timer_running) {
+    return kTvmErrorPlatformTimerBadState;
+  }
+  g_utvm_timer_running = 1;
+  g_utvm_start_time_micros = micros();
+  return kTvmErrorNoError;
+}
+
+tvm_crt_error_t TVMPlatformTimerStop(double* elapsed_time_seconds) {
+  if (!g_utvm_timer_running) {
+    return kTvmErrorPlatformTimerBadState;
+  }
+  g_utvm_timer_running = 0;
+  unsigned long g_utvm_stop_time = micros() - g_utvm_start_time_micros;
+  *elapsed_time_seconds = ((double)g_utvm_stop_time) / 1e6;
+  return kTvmErrorNoError;
+}
+
+// Ring buffer used to store data read from the UART on rx interrupt.
+// This ring buffer size is only required for testing with QEMU and not for physical hardware.
+//#define RING_BUF_SIZE_BYTES (TVM_CRT_MAX_PACKET_SIZE_BYTES + 100)
+//RING_BUF_ITEM_DECLARE_SIZE(uart_rx_rbuf, RING_BUF_SIZE_BYTES);
+
+// UART interrupt callback.
+/*void uart_irq_cb(const struct device* dev, void* user_data) {
+  uart_irq_update(dev);
+  if (uart_irq_is_pending(dev)) {
+    struct ring_buf* rbuf = (struct ring_buf*)user_data;
+    if (uart_irq_rx_ready(dev) != 0) {
+      uint8_t* data;
+      uint32_t size;
+      size = ring_buf_put_claim(rbuf, &data, RING_BUF_SIZE_BYTES);
+      int rx_size = uart_fifo_read(dev, data, size);
+      // Write it into the ring buffer.
+      g_num_bytes_in_rx_buffer += rx_size;
+
+      if (g_num_bytes_in_rx_buffer > RING_BUF_SIZE_BYTES) {
+        TVMPlatformAbort((tvm_crt_error_t)0xbeef3);
+      }
+
+      if (rx_size < 0) {
+        TVMPlatformAbort((tvm_crt_error_t)0xbeef1);
+      }
+
+      int err = ring_buf_put_finish(rbuf, rx_size);
+      if (err != 0) {
+        TVMPlatformAbort((tvm_crt_error_t)0xbeef2);
+      }
+      // CHECK_EQ(bytes_read, bytes_written, "bytes_read: %d; bytes_written: %d", bytes_read,
+      // bytes_written);
+    }
+  }
+}*/
+
+// Used to initialize the UART receiver.
+/*void uart_rx_init(struct ring_buf* rbuf, const struct device* dev) {
+  uart_irq_callback_user_data_set(dev, uart_irq_cb, (void*)rbuf);
+  uart_irq_rx_enable(dev);
+}*/
+
+// The main function of this application.
+//extern void __stdout_hook_install(int (*hook)(int));
+void main(void) {
+
+  // Claim console device.
+  //tvm_uart = device_get_binding(DT_LABEL(DT_CHOSEN(zephyr_console)));
+  //uart_rx_init(&uart_rx_rbuf, tvm_uart);
+
+  // Initialize microTVM RPC server, which will receive commands from the UART and execute them.
+  microtvm_rpc_server_t server = MicroTVMRpcServerInit(write_serial, NULL);
+  TVMLogf("microTVM ETISSVP runtime - running");
+
+  // The main application loop. We continuously read commands from the UART
+  // and dispatch them to MicroTVMRpcServerLoop().
+  while (true) {
+    uint8_t* data;
+    //unsigned int key = irq_lock();
+    //uint32_t bytes_read = ring_buf_get_claim(&uart_rx_rbuf, &data, RING_BUF_SIZE_BYTES);
+    /*if (bytes_read > 0) {
+      g_num_bytes_in_rx_buffer -= bytes_read;
+      size_t bytes_remaining = bytes_read;
+      while (bytes_remaining > 0) {
+        // Pass the received bytes to the RPC server.
+        tvm_crt_error_t err = MicroTVMRpcServerLoop(server, &data, &bytes_remaining);
+        if (err != kTvmErrorNoError && err != kTvmErrorFramingShortPacket) {
+          TVMPlatformAbort(err);
+        }
+        if (g_num_bytes_written != 0 || g_num_bytes_requested != 0) {
+          if (g_num_bytes_written != g_num_bytes_requested) {
+            TVMPlatformAbort((tvm_crt_error_t)0xbeef5);
+          }
+          g_num_bytes_written = 0;
+          g_num_bytes_requested = 0;
+        }
+      }
+      int err = ring_buf_get_finish(&uart_rx_rbuf, bytes_read);
+      if (err != 0) {
+        TVMPlatformAbort((tvm_crt_error_t)0xbeef6);
+      }
+    }*/
+    //irq_unlock(key);
+    break;
+  }
+  TVMLogf("microTVM ETISSVP runtime - done");
+  for (;;) {}
+
+}
