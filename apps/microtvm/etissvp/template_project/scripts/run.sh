227ecfe084daaf10670f724b52a705b383531d0e
Philipp van Kempen <philipp.van-kempen@tum.de>
2021-11-08 13:55:55 +0100

    ETISSVP: Do not parse metrics in run.sh

--- apps/microtvm/etissvp/template_project/scripts/run.sh
+++ apps/microtvm/etissvp/template_project/scripts/run.sh
@@ -19,8 +19,8 @@ $ETISS/examples/bare_etiss_processor/run_helper.sh "$@"
 ELF_FILE=$1
 #MEM_INI="../../out/memsegs.ini"
 # TODO: determine memory layout!
-echo "Metrics:"
-python3 $ETISS/examples/bare_etiss_processor/get_metrics.py "$ELF_FILE" -t "dBusAccess.csv" -o "metrics.csv"
+#echo "Metrics:"
+#python3 $ETISS/examples/bare_etiss_processor/get_metrics.py "$ELF_FILE" -t "dBusAccess.csv" -o "metrics.csv"
 echo "Done!"
-cat metrics.csv
-echo "Done2!"
+#cat metrics.csv
+#echo "Done2!"
