de8736098c3803b12ba7fc348bafbec5d1e9f63c
Philipp van Kempen <philipp.van-kempen@tum.de>
2021-11-08 13:49:06 +0100

    [TMP] enable RPC client framer logging

--- apps/microtvm/etissvp/template_project/crt_config/crt_config.h
+++ apps/microtvm/etissvp/template_project/crt_config/crt_config.h
@@ -62,6 +62,6 @@
 /*! \brief Number of pages on device. */
 #define TVM_CRT_MAX_PAGES 300
 
-// #define TVM_CRT_FRAMER_ENABLE_LOGS
+#define TVM_CRT_FRAMER_ENABLE_LOGS
 
 #endif  // TVM_RUNTIME_CRT_CONFIG_H_
