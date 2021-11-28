fc2a101e346b6488f40c315df94bd816f3716dfb
Philipp van Kempen <philipp.van-kempen@tum.de>
2021-11-28 12:09:15 +0100

    [ETISSVP] [WIP] fix run.sh script

--- apps/microtvm/etissvp/template_project/scripts/run.sh
+++ apps/microtvm/etissvp/template_project/scripts/run.sh
@@ -10,11 +10,11 @@ ETISS=${ETISS_DIR:-/work/git/prj/etiss_freertos/etiss-public-fork/build/installe
 #fi
 
 echo "<> Setting up ETISSVP..."
-echo -e "\n\n\r===================FIFO IN=========================\n" >> /tmp/etissvp_rx.log
-( until [ -p `pwd`/.tmp/uartdevicefifoin2 ] ; do sleep 1 ; done ; cat `pwd`/.tmp/uartdevicefifoin2 >> /tmp/etissvp_rx.log) &
-echo -e "\n\n\r===================FIFO OUT=========================\n" >> /tmp/etissvp_tx.log
-( until [ -p `pwd`/.tmp/uartdevicefifoout2 ] ; do test -d `pwd` && sleep 1 || exit ; done ; cat `pwd`/.tmp/uartdevicefifoout2 >> /tmp/etissvp_tx.log) &
-echo "Monitors for the virtual uart have been set up. Use 'tail -f /tmp/etissvp_rx.log' and 'tail -f /tmp/etissvp_tx.log' to inspect them."
+#echo -e "\n\n\r===================FIFO IN=========================\n" >> /tmp/etissvp_rx.log
+#( until [ -p `pwd`/.tmp/uartdevicefifoin2 ] ; do sleep 1 ; done ; cat `pwd`/.tmp/uartdevicefifoin2 >> /tmp/etissvp_rx.log) &
+#echo -e "\n\n\r===================FIFO OUT=========================\n" >> /tmp/etissvp_tx.log
+#( until [ -p `pwd`/.tmp/uartdevicefifoout2 ] ; do test -d `pwd` && sleep 1 || exit ; done ; cat `pwd`/.tmp/uartdevicefifoout2 >> /tmp/etissvp_tx.log) &
+#echo "Monitors for the virtual uart have been set up. Use 'tail -f /tmp/etissvp_rx.log' and 'tail -f /tmp/etissvp_tx.log' to inspect them."
 echo -e "\n\n\r===================ETISS=========================\n" >> /tmp/etissvp.log
 echo "Simulation logs will be available in /tmp/etissvp.log!"
 $ETISS/examples/bare_etiss_processor/run_helper.sh "$@" 2>&1 | tee -a /tmp/etissvp.log
