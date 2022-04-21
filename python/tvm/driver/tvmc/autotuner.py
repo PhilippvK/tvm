113d23f126ba5ba04d1425af9fa364686ec6b0ba
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-04-21 21:49:17 +0200

    [WIP] [TVMC] [MicroTVM] `tvmc micro tune`: Use si_prefix option

--- python/tvm/driver/tvmc/autotuner.py
+++ python/tvm/driver/tvmc/autotuner.py
@@ -315,6 +315,7 @@ def tune_model(
     build_func = "default",  # TODO
     runtime = None,  # TODO
     build_kwargs : dict = None,  # TODO
+    si_prefix : Optional[str] = "G",
 ):
     """Use tuning to automatically optimize the functions in a model.
 
@@ -380,6 +381,8 @@ def tune_model(
     runtime : TODO, optional
         TODO
     build_kwargs : dict, optional
+        TODO
+    si_prefix : str, optional
         TODO
 
     Returns
@@ -513,6 +516,7 @@ def tune_model(
                 builder=builder, runner=runner
             ),
             "tuning_records": prior_records,
+            "si_prefix": si_prefix,
         }
         logger.info("Autotuning with configuration: %s", tuning_options)
 
@@ -670,6 +674,7 @@ def tune_tasks(
     trials: int,
     early_stopping: Optional[int] = None,
     tuning_records: Optional[str] = None,
+    si_prefix: Optional[str] = "G",
 ):
     """Tune a list of tasks and output the history to a log file.
 
@@ -691,6 +696,8 @@ def tune_tasks(
     tuning_records: str, optional
         Path to the file produced by the tuning, to be used during
         tuning.
+    si_prefix : str
+        TODO
     """
     if not tasks:
         logger.warning("there were no tasks found to be tuned")
@@ -728,7 +735,8 @@ def tune_tasks(
             early_stopping=early_stopping,
             measure_option=measure_option,
             callbacks=[
-                autotvm.callback.progress_bar(trials, prefix=prefix),
+                autotvm.callback.progress_bar(trials, prefix=prefix, si_prefix=si_prefix),
                 autotvm.callback.log_to_file(log_file),
             ],
+            si_prefix=si_prefix,
         )
