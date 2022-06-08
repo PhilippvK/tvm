e7373ed4c28cacb6419eca1507abc7a06d0ef48b
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-06-08 16:40:21 +0200

    fix build function behavior

--- python/tvm/auto_scheduler/relay_integration.py
+++ python/tvm/auto_scheduler/relay_integration.py
@@ -47,7 +47,7 @@ from .workload_registry import register_workload_tensors
 logger = logging.getLogger("auto_scheduler")
 
 
-def call_all_topi_funcs(mod, params, target, error_list, opt_level=3):
+def call_all_topi_funcs(mod, params, target, error_list, opt_level=3, extra_config={}):
     """Call all TOPI compute to extract auto_scheduler tasks in a Relay program"""
     # pylint: disable=import-outside-toplevel
     from tvm import relay
@@ -60,6 +60,7 @@ def call_all_topi_funcs(mod, params, target, error_list, opt_level=3):
         opt_level=opt_level,
         config={
             "relay.backend.use_auto_scheduler": True,
+            **extra_config,
         },
         disabled_pass={"AutoSchedulerLayoutRewrite"},
     ):
@@ -84,6 +85,7 @@ def extract_tasks(
     include_simple_tasks=False,
     dump_workload_to_dag_log=None,
     opt_level=3,
+    extra_config={},
 ):
     """Extract tuning tasks from a relay program.
 
@@ -105,6 +107,8 @@ def extract_tasks(
         A file to dump an association between the workload keys and the actual DAG
     opt_level : Optional[int]
         The optimization level of the task extractions.
+    extra_config : dict
+        Custom confi passed to transform.PassContext
 
     Returns
     -------
@@ -130,7 +134,7 @@ def extract_tasks(
         # Wrap build call in a new thread to avoid the conflict
         # between python's multiprocessing and tvm's thread pool
         build_thread = threading.Thread(
-            target=call_all_topi_funcs, args=(mod, params, target, errors, opt_level)
+            target=call_all_topi_funcs, args=(mod, params, target, errors, opt_level, extra_config)
         )
         build_thread.start()
         build_thread.join()
