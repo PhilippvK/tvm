c59a1de5ddfb769e87a9cf4b4665cb5bcdd3036f
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-08-16 11:43:36 +0200

    [TVMC] add --disabled-pass argument to `tvmc tune`

--- python/tvm/driver/tvmc/autotuner.py
+++ python/tvm/driver/tvmc/autotuner.py
@@ -72,6 +72,12 @@ def add_tune_args(parser, micro=False):
         help="specify input model format",
     )
     parser.add_argument(
+        "--disabled-pass",
+        help="disable specific passes, comma-separated list of pass names.",
+        type=parse_pass_list_str,
+        default="",
+    )
+    parser.add_argument(
         "--number",
         default=1 if micro else 10,
         type=int,
@@ -361,6 +367,7 @@ def drive_tune(args):
         port=rpc_port,
         trials=args.trials,
         target_host=args.target_host,
+        disabled_pass=args.disabled_pass,
         tuner=args.tuner,
         min_repeat_ms=args.min_repeat_ms,
         early_stopping=args.early_stopping,
@@ -481,6 +488,7 @@ def tune_model(
     port: Optional[Union[int, str]] = 9090,
     trials: int = 10000,
     target_host: Optional[str] = None,
+    disabled_pass: Optional[str] = None,
     tuner: str = "xgb",
     min_repeat_ms: Optional[int] = None,
     early_stopping: Optional[int] = None,
@@ -531,6 +539,9 @@ def tune_model(
         The IP address of an RPC tracker, used when benchmarking remotely.
     port : int or str, optional
         The port of the RPC tracker to connect to. Defaults to 9090.
+    disabled_pass: str, optional
+        Comma-separated list of passes which needs to be disabled
+        during compilation
     trials : int, optional
         The number of schedules to try out for the entire model. Note that the default
         value is chosen as a decent average for most models, but larger models may need
@@ -617,7 +628,8 @@ def tune_model(
         config.update(extra_config)
     print("config", config)
 
-    with tvm.transform.PassContext(opt_level=3, config=config):
+    with tvm.transform.PassContext(opt_level=3, config=config, disabled_pass=disabled_pass):
+        mod = apply_graph_transforms(mod, transform_args)
         if tuning_records is None:
             tuning_records = tvmc_model.default_tuning_records_path()
 
