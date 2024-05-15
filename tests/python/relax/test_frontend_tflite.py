8294b68ae1d737e223a11c182d5aba6f15e17407
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-05-15 08:55:14 +0200

    update relax/test_frontend_tflite

--- tests/python/relax/test_frontend_tflite.py
+++ tests/python/relax/test_frontend_tflite.py
@@ -17,19 +17,188 @@
 
 # pylint: disable=c-extension-no-member
 
+import os
 import functools
 from typing import Union, Tuple, List
 import pytest
 import numpy as np
+from packaging import version as package_version
+
+from PIL import Image
 
 import tvm
 import tvm.testing
 import tvm.relay.testing.tf as tf_testing
-from tvm import relax
+from tvm.contrib.download import download_testdata
+from tvm import relax, relay
+from tvm.contrib import graph_executor
 from tvm.script import ir as I
 from tvm.script import tir as T
 from tvm.script import relax as R
 from tvm.relax.frontend.tflite import from_tflite
+
+try:
+    import tensorflow.compat.v1 as tf
+
+    # tensorflow.python.framework.ops module itself is not part of
+    # TensorFlow's public API: the precise contents of that module
+    # may vary from one version to the next
+    import tensorflow.compat.v1 as ops
+except ImportError:
+    import tensorflow as tf
+    import tensorflow as ops
+
+try:
+    from tensorflow import lite as interpreter_wrapper
+except ImportError:
+    from tensorflow.contrib import lite as interpreter_wrapper
+
+
+# TODO: share code with tflite relay frontend
+#######################################################################
+# Generic run functions for TVM & TFLite
+# --------------------------------------
+def convert_to_list(x):
+    if not isinstance(x, list):
+        x = [x]
+    return x
+#######################################################################
+# Get a real image for e2e testing
+# --------------------------------
+def get_real_image(im_height, im_width, quantized=True):
+    repo_base = "https://github.com/dmlc/web-data/raw/main/tensorflow/models/InceptionV1/"
+    img_name = "elephant-299.jpg"
+    image_url = os.path.join(repo_base, img_name)
+    img_path = download_testdata(image_url, img_name, module="data")
+    image = Image.open(img_path).resize((im_height, im_width))
+    x = np.array(image).astype("uint8") if quantized else np.array(image).astype("float32")
+    data = np.reshape(x, (1, im_height, im_width, 3))
+    return data
+
+
+def pre_processed_image(height, width):
+    """Image preprocessed"""
+    repo_base = "https://github.com/dmlc/web-data/raw/main/tensorflow/models/InceptionV1/"
+    img_name = "elephant-299.jpg"
+    image_url = os.path.join(repo_base, img_name)
+    img_path = download_testdata(image_url, img_name, module="data")
+    image = tf.io.read_file(img_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    with tf.name_scope("eval_image"):
+        if image.dtype != tf.float32:
+            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        image = tf.image.central_crop(image, central_fraction=0.875)
+    # Resize the image to the specified height and width.
+    image = tf.image.resize(image, [height, width], align_corners=False)
+    image = tf.expand_dims(image, axis=0)
+    return image
+
+def run_tvm_graph(
+    tflite_model_buf,
+    input_data,
+    input_node,
+    num_output=1,
+    target="llvm",
+    out_names=None,
+    mode="graph_executor",
+    op_converter=relay.frontend.tflite.OperatorConverter,
+):
+    """Generic function to compile on relay and execute on tvm"""
+    # TFLite.Model.Model has changed to TFLite.Model from 1.14 to 2.1
+    try:
+        import tflite.Model
+
+        tflite_model = tflite.Model.Model.GetRootAsModel(tflite_model_buf, 0)
+    except AttributeError:
+        import tflite
+
+        tflite_model = tflite.Model.GetRootAsModel(tflite_model_buf, 0)
+    except ImportError as exc:
+        raise ImportError("The tflite package must be installed") from exc
+
+    input_data = convert_to_list(input_data)
+    input_node = convert_to_list(input_node)
+
+    shape_dict = {}
+    dtype_dict = {}
+    for i, node in enumerate(input_node):
+        shape_dict[node] = input_data[i].shape
+        dtype_dict[node] = input_data[i].dtype.name
+
+    with tvm.testing.disable_span_filling():
+        mod, params = relay.frontend.from_tflite(
+            tflite_model, shape_dict=shape_dict, dtype_dict=dtype_dict, op_converter=op_converter
+        )
+    with tvm.testing.enable_span_filling():
+        mod_with_span, _ = relay.frontend.from_tflite(
+            tflite_model, shape_dict=shape_dict, dtype_dict=dtype_dict, op_converter=op_converter
+        )
+    assert tvm.ir.structural_equal(mod["main"], mod_with_span["main"])
+
+    if mode in ["debug", "vm"]:
+        inputs = []
+        for param in mod["main"].params:
+            found = False
+            for i, n in enumerate(input_node):
+                if n == param.name_hint:
+                    found = True
+                    inputs.append(tvm.nd.array(input_data[i]))
+                    break
+            # Interpreter doesn't bind constants, so still need to find in params
+            if not found:
+                inputs.append(tvm.nd.array(params[param.name_hint]))
+        result = relay.create_executor(mode, mod=mod, device=tvm.cpu(), target="llvm").evaluate()(
+            *inputs
+        )
+        return vmobj_to_list(result)
+    else:
+        with tvm.transform.PassContext(opt_level=3):
+            lib = relay.build(mod, target, params=params)
+
+        dev = tvm.device(target, 0)
+
+        m = graph_executor.GraphModule(lib["default"](dev))
+        # set inputs
+        for i, node in enumerate(input_node):
+            m.set_input(node, tvm.nd.array(input_data[i].astype(input_data[i].dtype)))
+        # execute
+        m.run()
+        # get outputs
+        assert out_names is None or num_output == len(
+            out_names
+        ), f"out_names: {out_names} num_output: {num_output}"
+        tvm_output_list = []
+        for i in range(0, num_output):
+            tvm_output = m.get_output(i)
+            tvm_output_list.append(tvm_output.numpy())
+        return tvm_output_list
+
+def run_tflite_graph(tflite_model_buf, input_data):
+    """Generic function to execute TFLite"""
+    input_data = convert_to_list(input_data)
+
+    interpreter = interpreter_wrapper.Interpreter(model_content=tflite_model_buf)
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    for i, input_detail in enumerate(input_details):
+        interpreter.resize_tensor_input(input_detail["index"], input_data[i].shape)
+    interpreter.allocate_tensors()
+
+    # set input
+    assert len(input_data) == len(input_details)
+    for i, input_detail in enumerate(input_details):
+        interpreter.set_tensor(input_detail["index"], input_data[i])
+
+    # Run
+    interpreter.invoke()
+
+    # get output
+    tflite_output = []
+    for _, output_detail in enumerate(output_details):
+        tflite_output.append(interpreter.get_tensor(output_detail["index"]))
+
+    return tflite_output
 
 
 # def generate_np_inputs(
@@ -137,32 +306,34 @@ from tvm.relax.frontend.tflite import from_tflite
 #         tvm.testing.assert_allclose(tvm_out.numpy(), jax_out, rtol=1e-5, atol=1e-5)
 #
 #
-# def get_vm_res(
-#     ir_mod: tvm.IRModule, weights: Union[np.ndarray, List[np.ndarray]]
-# ) -> Union[tvm.nd.NDArray, List[tvm.nd.NDArray]]:
-#     """Compile and run an ir_module on Relax VM
-#
-#     Parameters
-#     ----------
-#     ir_mod: tvm.IRModule
-#         input ir module
-#
-#     weights: Union[np.ndarray, List[np.ndarray]]
-#          input weights
-#
-#     Results
-#     -------
-#     out: Union[tvm.nd.NDArray, List[tvm.nd.NDArray]]
-#         inference result
-#     """
-#     target = tvm.target.Target("llvm", host="llvm")
-#     # Compile and run
-#     ex = relax.build(ir_mod, target)
-#     vm = relax.VirtualMachine(ex, tvm.cpu())
-#     vm.set_input("main", *weights)
-#     vm.invoke_stateful("main")
-#     tvm_output = vm.get_outputs("main")
-#     return tvm_output
+def get_vm_res(
+    ir_mod: tvm.IRModule, weights: Union[np.ndarray, List[np.ndarray]]
+) -> Union[tvm.nd.NDArray, List[tvm.nd.NDArray]]:
+    """Compile and run an ir_module on Relax VM
+
+    Parameters
+    ----------
+    ir_mod: tvm.IRModule
+        input ir module
+
+    weights: Union[np.ndarray, List[np.ndarray]]
+         input weights
+
+    Results
+    -------
+    out: Union[tvm.nd.NDArray, List[tvm.nd.NDArray]]
+        inference result
+    """
+    print("get_vm_res", weights)
+    input("!")
+    target = tvm.target.Target("llvm", host="llvm")
+    # Compile and run
+    ex = relax.build(ir_mod, target)
+    vm = relax.VirtualMachine(ex, tvm.cpu())
+    vm.set_input("main", *weights)
+    vm.invoke_stateful("main")
+    tvm_output = vm.get_outputs("main")
+    return tvm_output
 #
 #
 # @tvm.testing.requires_gpu
@@ -315,7 +486,7 @@ from tvm.relax.frontend.tflite import from_tflite
 #     check_correctness(jax.jit(fn), input_shapes)
 
 
-def test_tflite_model():
+def test_forward_mobilenet_v2():
     """Test the Mobilenet V2 TF Lite model."""
     # MobilenetV2
     tflite_model_file = tf_testing.get_workload_official(
@@ -344,11 +515,118 @@ def test_tflite_model():
     ir_mod = from_tflite(
         tflite_model, shape_dict=shape_dict, dtype_dict=dtype_dict,
     )
-    # ir_mod.show()
-    # compile and run
-    # tvm_output = get_vm_res(ir_mod, inputs_np)
+    return
+    # get ref output (tflite)
+    tflite_output = run_tflite_graph(tflite_model_buf, data)
+    # compile and run (relay)
+    tvm_output = run_tvm_graph(tflite_model_buf, data, "input")
+    tvm.testing.assert_allclose(
+        np.squeeze(tvm_output[0]), np.squeeze(tflite_output[0]), rtol=1e-5, atol=1e-5
+    )
+    # compile and run (relax)
+    # tvm_output = get_vm_res(ir_mod, data)
     # verify accuracy
     # tvm.testing.assert_allclose(tvm_output.numpy(), expected  _output, rtol=1e-5, atol=1e-5)
+
+
+@pytest.mark.skip("Relax QDQ does not support uint8")
+def test_forward_qnn_mobilenet_v2_net():
+    """Test the Quantized TFLite Mobilenet V2 model."""
+    # MobilenetV2
+    tflite_model_file = tf_testing.get_workload_official(
+        "https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/"
+        "mobilenet_v2_1.0_224_quant.tgz",
+        "mobilenet_v2_1.0_224_quant.tflite",
+    )
+    with open(tflite_model_file, "rb") as f:
+        tflite_model_buf = f.read()
+
+    # Test image. Checking the labels because the requantize implementation is different between
+    # TFLite and Relay. This cause final output numbers to mismatch. So, testing accuracy via
+    # labels. Also, giving a real image, instead of random inputs.
+    data = get_real_image(224, 224)
+
+    try:
+        import tflite.Model
+
+        tflite_model = tflite.Model.Model.GetRootAsModel(tflite_model_buf, 0)
+    except AttributeError:
+        import tflite
+
+        tflite_model = tflite.Model.GetRootAsModel(tflite_model_buf, 0)
+    except ImportError as exc:
+        raise ImportError("The tflite package must be installed") from exc
+
+    shape_dict = {"input": (1, 224, 224, 3)}
+    dtype_dict = {"input": "uint8"}
+    input_info = [(shape_dict[name], dtype_dict[name]) for name in shape_dict]
+
+    # convert in Relax
+    ir_mod = from_tflite(
+        tflite_model, shape_dict=shape_dict, dtype_dict=dtype_dict,
+    )
+    # tflite_output = run_tflite_graph(tflite_model_buf, data)
+    # tflite_predictions = np.squeeze(tflite_output)
+    # tflite_sorted_labels = tflite_predictions.argsort()[-3:][::-1]
+    # tvm_output = run_tvm_graph(tflite_model_buf, data, "input")
+    # tvm_predictions = np.squeeze(tvm_output)
+    # tvm_sorted_labels = tvm_predictions.argsort()[-3:][::-1]
+    # tvm.testing.assert_allclose(tvm_sorted_labels, tflite_sorted_labels)
+
+
+def test_forward_tflite2_qnn_mobilenet_v2():
+    """Test the Quantized TFLite version 2.1.0 Mobilenet V2 model."""
+    if package_version.parse(tf.VERSION) >= package_version.parse("2.1.0"):
+        tflite_model_file = download_testdata(
+            "https://raw.githubusercontent.com/dmlc/web-data/main/tensorflow/models/Quantized/"
+            "mobilenet_v2_quantized.tflite",
+            "mobilenet_v2_quantized.tflite",
+        )
+        with open(tflite_model_file, "rb") as f:
+            tflite_model_buf = f.read()
+
+        data = pre_processed_image(224, 224)
+
+        try:
+            import tflite.Model
+
+            tflite_model = tflite.Model.Model.GetRootAsModel(tflite_model_buf, 0)
+        except AttributeError:
+            import tflite
+
+            tflite_model = tflite.Model.GetRootAsModel(tflite_model_buf, 0)
+        except ImportError as exc:
+            raise ImportError("The tflite package must be installed") from exc
+
+        shape_dict = {"input": (1, 224, 224, 3)}
+        dtype_dict = {"input": "float32"}
+        input_info = [(shape_dict[name], dtype_dict[name]) for name in shape_dict]
+
+        # convert in Relax
+        ir_mod = from_tflite(
+            tflite_model, shape_dict=shape_dict, dtype_dict=dtype_dict,
+        )
+        # return
+        tvm_output = get_vm_res(ir_mod, np.array(data))
+
+        # get ref output (tflite)
+        # tvm_output = run_tvm_graph(tflite_model_buf, np.array(data), "input_1")
+        tflite_output = run_tflite_graph(tflite_model_buf, data)
+        tflite_predictions = np.squeeze(tflite_output)
+        tflite_sorted_labels = tflite_predictions.argsort()[-3:][::-1]
+        # compile and run (relay)
+        tvm_output = run_tvm_graph(tflite_model_buf, np.array(data), "input_1")
+        tvm_predictions = np.squeeze(tvm_output)
+        tvm_sorted_labels = tvm_predictions.argsort()[-3:][::-1]
+        # tvm.testing.assert_allclose(
+        #     np.squeeze(tvm_output[0]), np.squeeze(tflite_output[0]), rtol=1e-5, atol=1e-5
+        # )
+        tvm.testing.assert_allclose(tvm_sorted_labels, tflite_sorted_labels)
+        # compile and run (relax)
+        tvm_output = get_vm_res(ir_mod, np.array(data))
+        # verify accuracy
+        # tvm.testing.assert_allclose(tvm_output.numpy(), expected  _output, rtol=1e-5, atol=1e-5)
+
 
 
 if __name__ == "__main__":
