8efde0eb39d63d42f8d9883607d92d6af709cc3d
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-04 08:48:20 +0200

    add micro specific tests

--- tests/python/driver/tvmc/test_runner.py
+++ tests/python/driver/tvmc/test_runner.py
@@ -110,6 +110,42 @@ def test_run_tflite_module__with_profile__valid_input(
     assert "output_0" in result.outputs.keys()
 
 
+def test_run_tflite_module__with_profile__valid_input_micro(
+    tflite_mobilenet_v1_1_quant, tflite_compile_model, imagenet_cat
+):
+    # some CI environments wont offer TFLite, so skip in case it is not present
+    pytest.importorskip("tflite")
+
+    inputs = np.load(imagenet_cat)
+    input_dict = {"input": inputs["input"].astype("uint8")}
+
+    tflite_compiled_model = tflite_compile_model(tflite_mobilenet_v1_1_quant, use_vm=False)
+    result = tvmc.run(
+        tflite_compiled_model,
+        inputs=input_dict,
+        benchmark=True,
+        hostname=None,
+        device="micro",
+        profile=True,
+    )
+
+    # collect the top 5 results
+    top_5_results = get_top_results(result, 5)
+    top_5_ids = top_5_results[0]
+
+    # IDs were collected from this reference:
+    # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/
+    # java/demo/app/src/main/assets/labels_mobilenet_quant_v1_224.txt
+    tiger_cat_mobilenet_id = 283
+
+    assert (
+        tiger_cat_mobilenet_id in top_5_ids
+    ), "tiger cat is expected in the top-5 for mobilenet v1"
+    assert isinstance(result.outputs, dict)
+    assert isinstance(result.times, BenchmarkResult)
+    assert "output_0" in result.outputs.keys()
+
+
 def test_run_tflite_module_with_rpc(
     tflite_mobilenet_v1_1_quant, tflite_compile_model, imagenet_cat
 ):
@@ -131,6 +167,44 @@ def test_run_tflite_module_with_rpc(
         hostname=server.host,
         port=server.port,
         device="cpu",
+    )
+
+    top_5_results = get_top_results(result, 5)
+    top_5_ids = top_5_results[0]
+
+    # IDs were collected from this reference:
+    # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/
+    # java/demo/app/src/main/assets/labels_mobilenet_quant_v1_224.txt
+    tiger_cat_mobilenet_id = 283
+
+    assert (
+        tiger_cat_mobilenet_id in top_5_ids
+    ), "tiger cat is expected in the top-5 for mobilenet v1"
+    assert isinstance(result.outputs, dict)
+    assert "output_0" in result.outputs.keys()
+
+
+def test_run_tflite_module_with_rpc_micro(
+    tflite_mobilenet_v1_1_quant, tflite_compile_model, imagenet_cat
+):
+    """
+    Test to check that TVMC run is functional when it is being used in
+    conjunction with an RPC server.
+    """
+    pytest.importorskip("tflite")
+
+    inputs = np.load(imagenet_cat)
+    input_dict = {"input": inputs["input"].astype("uint8")}
+
+    tflite_compiled_model = tflite_compile_model(tflite_mobilenet_v1_1_quant)
+
+    server = rpc.Server("127.0.0.1", 9099)
+    result = tvmc.run(
+        tflite_compiled_model,
+        inputs=input_dict,
+        hostname=server.host,
+        port=server.port,
+        device="micro",
     )
 
     top_5_results = get_top_results(result, 5)
@@ -176,6 +250,45 @@ def test_run_relay_module__benchmarking(
         inputs=input_dict,
         hostname=None,
         device="cpu",
+        benchmark=benchmark,
+        repeat=repeat,
+        number=number,
+    )
+
+    # When no benchmarking is used, an empty list is used to
+    # represent an absence of results.
+    if isinstance(result.times, list):
+        assert len(result.times) == expected_len
+    else:
+        assert len(result.times.results) == expected_len
+
+
+@pytest.mark.parametrize(
+    "benchmark,repeat,number,expected_len", [(False, 1, 1, 0), (True, 1, 1, 1), (True, 3, 2, 3)]
+)
+def test_run_relay_module__benchmarking_micro(
+    benchmark,
+    repeat,
+    number,
+    expected_len,
+    relay_text_conv2d,
+    relay_compile_model,
+):
+    """Check the length of the results from benchmarking is what is expected by expected_len."""
+    shape_dict = {"data": (1, 3, 64, 64), "weight": (3, 3, 5, 5)}
+    input_dict = {
+        "data": np.random.randint(low=0, high=10, size=shape_dict["data"], dtype="uint8"),
+        "weight": np.random.randint(low=0, high=10, size=shape_dict["weight"], dtype="int8"),
+    }
+
+    tflite_compiled_model = relay_compile_model(
+        relay_text_conv2d, shape_dict=shape_dict, use_vm=False
+    )
+    result = tvmc.run(
+        tflite_compiled_model,
+        inputs=input_dict,
+        hostname=None,
+        device="micro",
         benchmark=benchmark,
         repeat=repeat,
         number=number,
