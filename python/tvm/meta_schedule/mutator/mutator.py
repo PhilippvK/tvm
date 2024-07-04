093163c0cfa9a62c63bb31f78ffd1b31d52e4e34
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-04 06:58:22 +0200

    ms: add micro mutator

--- python/tvm/meta_schedule/mutator/mutator.py
+++ python/tvm/meta_schedule/mutator/mutator.py
@@ -77,6 +77,7 @@ class Mutator(Object):
     def create(
         kind: Literal[
             "llvm",
+            "micro",
             "cuda",
             "cuda-tensorcore",
             "hexagon",
@@ -86,7 +87,7 @@ class Mutator(Object):
 
         Parameters
         ----------
-        kind : Literal["llvm", "cuda", "cuda-tensorcore", "hexagon"]
+        kind : Literal["llvm", "micro", "cuda", "cuda-tensorcore", "hexagon"]
             The kind of mutators.
 
         Returns
@@ -97,6 +98,7 @@ class Mutator(Object):
         funcs = {
             # pylint: disable=no-member
             "llvm": _ffi_api.MutatorDefaultLLVM,  # type: ignore
+            "micro": _ffi_api.MutatorDefaultMicro,  # type: ignore
             "cuda": _ffi_api.MutatorDefaultCUDA,  # type: ignore
             "cuda-tensorcore": _ffi_api.MutatorDefaultCUDATensorCore,  # type: ignore
             "hexagon": _ffi_api.MutatorDefaultHexagon,  # type: ignore
