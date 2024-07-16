af4cf0a49f3dd701a8b5f9be61a54e99f4c0c593
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-16 15:17:07 +0200

    update query_ms_db.py script

--- query_ms_db.py
+++ query_ms_db.py
@@ -6,6 +6,7 @@ from types import MappingProxyType
 from collections import defaultdict
 
 import numpy as np
+import pandas as pd
 
 import tvm
 from tvm import relay, tir
@@ -18,11 +19,16 @@ from tvm.tir.analysis import estimate_tir_flops
 parser = argparse.ArgumentParser(description="TODO")
 parser.add_argument("--workload", default="conv2d_relay", help="TODO")
 parser.add_argument("--database", "--db", default=None, help="TODO")
+parser.add_argument("--out", "-o", default=None, help="TODO")
 parser.add_argument("--print-ir-mod", action="store_true", help="TODO")
 parser.add_argument("--trivial-schedule", action="store_true", help="TODO")
 parser.add_argument("--fallback-schedule", action="store_true", help="TODO")
-parser.add_argument("--database-trials", "--trials", type=int, action="append", default=[], help="TODO")
+parser.add_argument("--database-trials", "--trials", type=int, nargs="+", default=[], help="TODO")
 parser.add_argument("--database-topk", "--topk", type=int, default=0, help="TODO")
+parser.add_argument("--project-options", default=None, help="TODO")
+parser.add_argument("--runtime", default="crt", help="TODO")
+parser.add_argument("--executor", default="aot", help="TODO")
+parser.add_argument("--alter-op", action="store_true", help="TODO")
 args = parser.parse_args()
 
 # TODO: move to workloads.py
@@ -109,20 +115,27 @@ if args.database:
     assert len(main_workloads) == 1
     main_workload = main_workloads[0]
     records_ = workload2records[main_workload]
-    for trial_idx in args.database_trials:
-        if trial_idx >= len(records_):
-            break
-        record = records_[trial_idx]
-        schedules.append(("meta_schedule", record, trial_idx))
+    records_by_time = sorted(records_, key=lambda x: x.timestamp)
+    print("records_", records_)
+    if args.database_trials:
+        for trial_idx in args.database_trials:
+            if trial_idx >= len(records_by_time):
+                break
+            record = records_by_time[trial_idx]
+            schedules.append(("meta_schedule", record, trial_idx))
     if args.database_topk:
         topk_records = database.get_top_k(main_workload, args.database_topk)
+        print("topk_records", topk_records)
         for k in range(min(len(topk_records), args.database_topk)):
             record = topk_records[k]
-            trial_idx = records_.index(record)
+            print("record", record)
+            print("record.run_secs", record.run_secs)
+            trial_idx = records_by_time.index(record)
+            print("trial_idx", trial_idx)
             schedules.append(("meta_schedule", record, trial_idx))
 
 print("schedules", schedules)
-input("1")
+# input("1")
 
 
 def _schedule_trivial(sch):
@@ -138,146 +151,68 @@ def _schedule_trivial(sch):
     # sch.reorder(i_0, j_0, i_1, j_1, k_0, i_2, j_2, k_1, i_3, j_3)
 
 
+def _schedule_dummy():
+
+    def schedule_fn(sch, block=None) -> bool:
+        return True
+
+    return schedule_fn
+
+
 def _create_schedule(mod, sch_fn):
     sch = tir.Schedule(mod=mod, debug_mask="all")
     sch_fn(sch)
     return sch
-
-
-
-
-assert len(sys.argv) == 2
-db_path = Path(sys.argv[1])
-path_workload = db_path / "database_workload.json"
-path_tuning_record = db_path / "database_tuning_record.json"
-# mod = Matmul
-mod, params, model_info = create_relay_module()
-# print("mod", mod)
-
-link_params = True
 
 if args.runtime == "crt":
     runtime = relay.backend.Runtime("crt", {"system-lib": True})
 else:
     raise ValueError(f"Unsupported runtime: {args.runtime}")
 if args.executor == "aot":
+    link_params = True
     executor = Executor("aot", {"link-params": link_params})
 else:
     raise ValueError(f"Unsupported executor: {args.executor}")
 
 # This line is necessary for link-params to take effect during
 # task extraction and relay.build(...).
-mod = mod.with_attr("executor", executor)
+ir_mod = ir_mod.with_attr("executor", executor)
 # print("mod2", mod)
 
-# trace = _create_schedule(mod, _schedule_matmul).trace
-database = ms.database.JSONDatabase(path_workload=str(path_workload), path_tuning_record=str(path_tuning_record))
-print("database", database, dir(database))
-# token = database.commit_workload(mod)
-# workload = database.commit_workload(mod)
-# ret = database.get_top_k(token, 2)
-# ret = database.get_top_k(workload, 2)
-# assert len(ret) == 2
-# target = tvm.target.Target("llvm")
 target = tvm.target.Target("c")
 print("target", target)
-record = database.query_tuning_record(mod=mod, target=target, workload_name="main")
-print("record", record)
-x = database.get_all_tuning_records()
-print("x", x)
-y = database.has_workload(mod)
-print("y", y)
-l = len(database)
-print("l", l)
-# print("x[0]", x[0], dir(x[0]))
-# print("x[0].run_secs", x[0].run_secs)
-# print("x[0].target", x[0].target)
-# print("x[0].trace", x[0].trace)
-# print("x[0].workload", x[0].workload, dir(x[0].workload))
-# print("x[0].workload.mod", x[0].workload.mod, dir(x[0].workload.mod))
-# print("x[1]", x[1], dir(x[1]))
-# print("x[1].run_secs", x[1].run_secs)
-# print("x[1].target", x[1].target)
-# print("x[1].trace", x[1].trace)
-# print("x[1].workload", x[1].workload, dir(x[1].workload))
-# print("x[1].workload.mod", x[1].workload.mod, dir(x[1].workload.mod))
-print("x[2]", x[2], dir(x[2]))
-print("x[2].run_secs", x[2].run_secs)
-print("x[2].target", x[2].target)
-print("x[2].trace", x[2].trace)
-print("x[2].workload", x[2].workload, dir(x[2].workload))
-print("x[2].workload.mod", x[2].workload.mod, dir(x[2].workload.mod))
-workload2 = x[2].workload
-# record = database.query_tuning_record(mod=mod, target=target, workload_name="main")
-# print("record", record)
-x2 = database.get_all_tuning_records()
-print("x2", x2)
-# y2 = database.has_workload(workload2)
-# print("y2", y2)
-TOP = 3
-topk = database.get_top_k(workload2, TOP)
-print("topk", topk)
-# workload = database.commit_workload(mod)
-# as_json
-# as_measure_candidate
-# handle
 
-alter_op = True
 pass_config = {
     "tir.disable_vectorize": True
 }
 disabled_pass = []
-if not alter_op:
+if not args.alter_op:
     disabled_pass += ["AlterOpLayout"]
-#
-# # ms_mod: tvm.runtime.Module = ms.relay_integration.compile_relay(
-# #     database=database,
-# #     # database=None,
-# #     mod=mod,
-# #     target=target,
-# #     params=params,
-# #     pass_config=MappingProxyType(
-# #         {
-# #             **pass_config,
-# #             "relay.backend.use_meta_schedule": True,
-# #             "relay.backend.tir_converter": "default",
 # #             # "relay.backend.use_meta_schedule_dispatch": 2,
-# #             # "tir.disable_vectorize": True,
-# #             # "tir.enable_debug": True,
-# #         }
-# #     ),
-# #     disabled_pass=disabled_pass,
-# #     executor=executor,
-# #     runtime=runtime,
-# # )
-# # print("ms_mod", ms_mod)
-# #
-# # with database, tvm.transform.PassContext(
-# #     opt_level=3,
-# #     config={
-# #         **pass_config,
-# #         "relay.backend.use_meta_schedule": True,
-# #         "relay.backend.use_meta_schedule_dispatch": 2,
-# #     },
-# # ):
-# #     lib = relay.build(mod, target=target, params=params, runtime=runtime, executor=executor)
 
 
-def eval_single_record(mod, record, target, pass_config, disabled_pass, runtime, executor, platform, options):
-    # with tempfile.TemporaryDirectory() as tmpdir:
-    # path_workload = osp.join(tmpdir, "workloads.json")
-    # path_tuning_record = osp.join(tmpdir, "tuning_records.json")
-    # temp_db = ms.database.JSONDatabase(path_workload, path_tuning_record, module_equality=mod_eq)
-    # mod_eq = "structural"
-    # flops = estimate_tir_flops(mod)
-    flops_ = estimate_tir_flops(record.workload.mod)
-    # print("flops", flops, flops_)
-    mem_db = ms.database.MemoryDatabase()
-    _ = mem_db.commit_workload(record.workload.mod)
-    mem_db.commit_tuning_record(record)
-    # TODO: skip db and just use mod from trace/workload?
+def eval_single_record(mod, mode, record, target, pass_config, disabled_pass, runtime, executor, platform, options, dispatch=1, skip_io=True):
+    if mode == "trivial":
+        assert record is None
+        flops_ = None
+        db = ms.database.ScheduleFnDatabase(
+            _schedule_dummy()
+        )
+    elif mode == "fallback":
+        assert record is None
+        flops_ = None
+        db = None
+    elif mode == "meta_schedule":
+        assert record is not None
+        flops_ = estimate_tir_flops(record.workload.mod)
+        db = ms.database.MemoryDatabase()
+        _ = db.commit_workload(record.workload.mod)
+        db.commit_tuning_record(record)
+        # TODO: skip db and just use mod from trace/workload?
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
     ms_mod: tvm.runtime.Module = ms.relay_integration.compile_relay(
-        database=mem_db,
+        database=db,
         mod=mod,
         target=target,
         params=params,
@@ -286,16 +221,13 @@ def eval_single_record(mod, record, target, pass_config, disabled_pass, runtime,
                 **pass_config,
                 "relay.backend.use_meta_schedule": True,
                 "relay.backend.tir_converter": "default",
-                # "relay.backend.use_meta_schedule_dispatch": 2,
-                # "tir.disable_vectorize": True,
-                # "tir.enable_debug": True,
+                "relay.backend.use_meta_schedule_dispatch": dispatch,
             }
         ),
         disabled_pass=disabled_pass,
         executor=executor,
         runtime=runtime,
     )
-    print("ms_mod", ms_mod)
 
     with tempfile.TemporaryDirectory() as tmpdir:
         work_dir = Path(tmpdir)
@@ -309,49 +241,62 @@ def eval_single_record(mod, record, target, pass_config, disabled_pass, runtime,
         project.flash()
         with tvm.micro.Session(project.transport()) as session:
             aot_executor = tvm.runtime.executor.aot_executor.AotModule(session.create_aot_executor())
-            # aot_executor.get_input(0).copyfrom(data_sample)
-            # result = aot_executor.module.time_evaluator("run", session.device, number=3)()
+            if not skip_io:
+                data_sample = None  # TODO
+                aot_executor.get_input(0).copyfrom(data_sample)
             result = aot_executor.module.time_evaluator("run", session.device, number=1)()
             print("result", result)
             print("mean: ", result.mean)
-            # output = aot_executor.get_output(0).numpy()
-        flops_per_s = flops_ / result.mean
-        return result.mean, flops_, flops_per_s
+            if not skip_io:
+                output = aot_executor.get_output(0).numpy()
+                print(output)
+        return result.mean, flops_
 
 
-toolchain = "gcc"
-platform = "/work/git/tvmtests/microtvm-etiss-template/template_project"
-project_options = {
-    "verbose": True,
-    "quiet": True,
-    # "gcc_prefix": "/var/tmp/ga87puy/microtvm/mlonmcu/workspace_default/deps/install/riscv_gcc",
-    "gcc_prefix": "/tmp/riscv_tools_rv64imfd_lp64d_medany/gnu",
-    # "gcc_name": "riscv32-unknown-elf",
-    "gcc_name": "riscv64-unknown-elf",
-    # "llvm_dir": "/var/tmp/ga87puy/microtvm/mlonmcu/workspace_default/deps/install/llvm",
-    "llvm_dir": "/tmp/seal5_llvm_corev/.seal5/build/release_assertions",
-    # "toolchain": "gcc",
-    # "toolchain": "llvm",
-    "toolchain": toolchain,
-    # "etiss_script": "/var/tmp/ga87puy/microtvm/mlonmcu/workspace_default/deps/install/etiss/bin/run_helper.sh",
-    "etiss_script": "/work/git/mlonmcu/mlonmcu/workspace_default/deps/install/etiss/bin/run_helper.sh",
-    "etiss_args": "",
-    # "arch": "rv32gc_zicsr_zifencei",
-    "arch": "rv64imfd",
-    # "abi": "ilp32d",
-    "abi": "lp64d",
-    # "cpu_arch": "RV32IMACFD",
-    "cpu_arch": "RV64IMACFD",
-    "cpu_freq": 100000000,
-}
+if args.project_options is None:
+    # use default
+    toolchain = "gcc"
+    platform = "/work/git/tvmtests/microtvm-etiss-template/template_project"
+    project_options = {
+        "verbose": True,
+        "quiet": True,
+        # "gcc_prefix": "/var/tmp/ga87puy/microtvm/mlonmcu/workspace_default/deps/install/riscv_gcc",
+        "gcc_prefix": "/tmp/riscv_tools_rv64imfd_lp64d_medany/gnu",
+        # "gcc_name": "riscv32-unknown-elf",
+        "gcc_name": "riscv64-unknown-elf",
+        # "llvm_dir": "/var/tmp/ga87puy/microtvm/mlonmcu/workspace_default/deps/install/llvm",
+        "llvm_dir": "/tmp/seal5_llvm_corev/.seal5/build/release_assertions",
+        # "toolchain": "gcc",
+        # "toolchain": "llvm",
+        "toolchain": toolchain,
+        # "etiss_script": "/var/tmp/ga87puy/microtvm/mlonmcu/workspace_default/deps/install/etiss/bin/run_helper.sh",
+        "etiss_script": "/work/git/mlonmcu/mlonmcu/workspace_default/deps/install/etiss/bin/run_helper.sh",
+        "etiss_args": "",
+        # "arch": "rv32gc_zicsr_zifencei",
+        "arch": "rv64imfd",
+        # "abi": "ilp32d",
+        "abi": "lp64d",
+        # "cpu_arch": "RV32IMACFD",
+        "cpu_arch": "RV64IMACFD",
+        "cpu_freq": 100000000,
+    }
+else:
+    raise NotImplementedError("load options from yaml")
 
-res = []
-res2 = []
-for tk in topk:
-    res_, _, res2_ = eval_single_record(mod, tk, target, pass_config, disabled_pass, runtime, executor, platform, project_options)
-    res.append(res_)
-    res2.append(res2_ / 1e6)
+df_data = []
+for schedule in schedules:
+    mode, record, trial_idx = schedule
+    mean_s, flops = eval_single_record(ir_mod, mode, record, target, pass_config, disabled_pass, runtime, executor, platform, project_options)
+    df_data_ = {"mode": mode, "record": trial_idx, "mean_s": mean_s, "flops": flops}
+    df_data.append(df_data_)
 
-print("topk!", [x.run_secs for x in topk])
-print("res", res)
-print("res2", res2)
+df = pd.DataFrame(df_data)
+unique_flops = df["flops"].dropna().unique()
+print("unique_flops", unique_flops)
+assert len(unique_flops) == 1
+df["flops"].fillna(unique_flops[0], inplace=True)
+df["flops_per_s"] = df["flops"] / df["mean_s"]
+print("df")
+print(df)
+if args.out:
+    df.to_csv(args.out, index=False)
