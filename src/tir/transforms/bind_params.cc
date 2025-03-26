9105d172c053ee20a96945505a594b321a1fc6a7
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-03-26 13:45:57 +0100

    src/tir/transforms/bind_params.cc: experiment with binparams for usmp compatibility with relax

--- src/tir/transforms/bind_params.cc
+++ src/tir/transforms/bind_params.cc
@@ -130,6 +130,58 @@ PrimFunc BindParams(PrimFunc f, const Array<runtime::NDArray>& constants) {
   return f;
 }
 
+PrimFunc BindParams2(PrimFunc f, const Map<String, runtime::NDArray>& constants2) {
+  LOG(INFO) << "BindParams2";
+  Map<tir::Var, runtime::NDArray> constant_map;
+  // Array<runtime::NDArray> constants;
+  Array<tir::Var> params;
+
+  auto* n = f.CopyOnWrite();
+  for (const tir::Var& var : n->params) {
+    String var_name = var->name_hint;
+    LOG(INFO) << "var_name" << var_name;
+    auto it = constants2.find(var_name);
+    if (it != constants2.end()) {
+        LOG(INFO) << "FOUND";
+        // runtime::NDArray data = constants2[var_name];
+        // constants.push_back(data);
+        tir::Var b = n->buffer_map[var]->data;
+        n->buffer_map.erase(var);
+        constant_map.Set(b, constants2[var_name]);
+    } else {
+        LOG(INFO) << "NOT FOUND";
+        params.push_back(var);
+    }
+  }
+  // return BindParams(f, constants);
+  n->params = params;
+  auto constant_list = ParamsCollector(constant_map).CollectParams(n->body);
+
+  // Allocate constants within the primfunc
+  for (auto i : constant_list) {
+    auto var = GetRef<Var>(i);
+    int ndim = constant_map[var]->ndim;
+    Array<PrimExpr> extents;
+
+    for (int i = 0; i < ndim; i++) {
+      int shape = constant_map[var]->shape[i];
+      extents.push_back(make_const(DataType::Int(32), shape));
+    }
+    DataType dtype = DataType(constant_map[var]->dtype);
+
+    if (n->body->IsInstance<BlockRealizeNode>()) {
+      auto* block_realize = n->body.as<BlockRealizeNode>();
+      auto block = block_realize->block;
+      block.CopyOnWrite()->body =
+          tir::AllocateConst(var, dtype, extents, constant_map[var], block->body);
+      n->body = BlockRealize(block_realize->iter_values, block_realize->predicate, block);
+    } else {
+      n->body = tir::AllocateConst(var, dtype, extents, constant_map[var], n->body);
+    }
+  }
+  return f;
+}
+
 namespace transform {
 
 Pass BindParams(const Array<runtime::NDArray>& constants) {
@@ -138,7 +190,59 @@ Pass BindParams(const Array<runtime::NDArray>& constants) {
   };
   return CreatePrimFuncPass(pass_func, 0, "tir.BindParams", {});
 }
-}  // namespace transform
 
+TVM_REGISTER_GLOBAL("tir.transform.BindParams")
+    .set_body_typed(BindParams);
+
+Pass BindParams2(const Map<String, runtime::NDArray>& constants) {
+  auto pass_func = [=](PrimFunc f, IRModule m, PassContext ctx) {
+    return BindParams2(f, constants);
+  };
+  return CreatePrimFuncPass(pass_func, 0, "tir.BindParams2", {});
+}
+
+TVM_REGISTER_GLOBAL("tir.transform.BindParams2")
+    .set_body_typed(BindParams2);
+
+Pass BindParams3(const Map<String, runtime::NDArray>& constants) {
+  auto pass_func = [=](IRModule mod, PassContext ctx) {
+
+    for (const auto& [gvar, base_func] : mod->functions) {
+      if (auto opt = base_func.as<PrimFunc>()) {
+        String func_name = gvar->name_hint;
+        Map<String, runtime::NDArray> filtered;
+        for (auto const& [name, data] : constants) {
+          std::string name_str = name;
+          auto pos = name_str.find(".");
+          if (pos != std::string::npos) {
+            std::string func = name_str.substr(0, pos);
+            LOG(INFO) << "func_name=" << func_name;
+            std::string rest = name_str.substr(pos + 1, std::string::npos);
+            LOG(INFO) << "rest=" << rest;
+            if (func_name != func) {
+              LOG(INFO) << "CONTINUE";
+              continue;
+            }
+            LOG(INFO) << "MATCH";
+            filtered.Set(rest, data);
+          }
+        }
+        LOG(INFO) << "filtered.size()=" << filtered.size();
+        if (filtered.size() > 0) {
+          auto new_func = BindParams2(opt.value(), filtered);
+          mod->Remove(gvar);
+          mod->Add(gvar, new_func);
+        }
+      }
+    }
+
+    return mod;
+  };
+  return tvm::transform::CreateModulePass(pass_func, 0, "tir.BindParams3", {});
+}
+
+TVM_REGISTER_GLOBAL("tir.transform.BindParams3").set_body_typed(BindParams3);
+
+}  // namespace transform
 }  // namespace tir
 }  // namespace tvm
