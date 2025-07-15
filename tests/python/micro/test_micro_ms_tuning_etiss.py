b06c577dbc51c7deabb8c85f23ac944e2da4b63b
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-07-15 15:13:37 +0200

    tests/python/micro/test_micro_ms_tuning_etiss.py: update

--- tests/python/micro/test_micro_ms_tuning_etiss.py
+++ tests/python/micro/test_micro_ms_tuning_etiss.py
@@ -19,6 +19,7 @@ from pathlib import Path
 import numpy as np
 import pytest
 from types import MappingProxyType
+from typing import Optional
 import tvm
 import tvm.testing
 from tvm import relay
@@ -26,6 +27,7 @@ from tvm.relay.backend import Executor
 # from tvm.contrib import graph_executor
 from tvm.contrib import utils
 from tvm import meta_schedule as ms
+from tvm.driver import tvmc
 
 ###
 import tvm.micro.testing
@@ -37,8 +39,258 @@ from tvm.meta_schedule.runner import EvaluatorConfig
 import logging
 logging.basicConfig(level=logging.ERROR)
 
+from tvm.meta_schedule.logging import get_logger
+get_logger("xgb_model").setLevel(logging.ERROR)
+
 DIR = Path(__file__).parent.resolve()
 BASE_DIR = DIR / "../../../../"
+
+
+MS_DISPATCH = 1  # silent?
+# MS_DISPATCH = 2  # verbose
+# MS_DISPATCH = ?  # error
+
+
+import numpy as np
+import pytest
+from types import MappingProxyType
+import pathlib
+import json
+import tvm
+import tvm.testing
+from tvm import relay
+from tvm import transform
+from tvm.relay.backend import Executor
+from tvm.contrib import graph_executor, utils
+from tvm import meta_schedule as ms
+from tvm.meta_schedule.utils import derived_object
+from tvm.tir.schedule import Schedule, Trace
+from tvm.tir.tensor_intrin.rocm import AMDGPU_SDOT4_INTRIN
+from tvm.tir.tensor_intrin.arm_cpu import DP4A_S8S8S32_INTRIN
+from tvm.tir.tensor_intrin.hexagon import VRMPY_i8i8i32_INTRIN
+from tvm.tir.tensor_intrin.arm_cpu import ARM_DOT_4x4_i8_NEON_INTRIN
+# from tvm.tir.tensor_intrin.cfu import CFU_MAC_i8i8i32_INTRIN
+
+from tvm.tir.schedule.analysis import has_block
+
+C_CODE = """
+#ifndef CFU_KERNEL_CODE
+#define CFU_KERNEL_CODE
+#include <stdint.h>
+
+#define MODE MODE_EMUL
+
+#if MODE == MODE_EMUL
+typedef struct {
+    uint32_t word0;
+    uint32_t word1;
+} weights_t;
+
+typedef struct {
+    int8_t x[4];
+} codebook_t;
+
+static weights_t current_weights = {.word0 = 0, .word1 = 0};
+// static codebook_t current_codebook = {.byte0 = 0, .byte1 = 0, .byte2 = 0, .byte3 = 0};
+static codebook_t current_codebook = {.x = {0, 0, 0, 0}};
+
+static int32_t acc = 0;
+#endif  // MODE
+
+#if MODE == MODE_EMUL
+static void __attribute__((always_inline)) inline push_weights_4b(uint32_t word0, uint32_t word1) {
+    // printf("push_weights_4b\\n");
+    current_weights.word0 = word0;
+    current_weights.word1 = word1;
+}
+#elif MODE == MODE_CFU
+static int32_t __attribute__((always_inline)) inline push_weights_4b(uint32_t word0, uint32_t word1) {
+    // printf("push_weights_4b\\n");
+#ifdef SEAL5
+    return __builtin_riscv_xcfu_cfu0_push_weights_4b(word0);
+#else
+    cfu_op0_hw(CFU_OPCODE_PUSH_WEIGHTS_4B, word0, word1);
+#endif  // SEAL5
+}
+#endif  // MODE
+
+static int32_t __attribute__((always_inline)) inline alu_mac(uint32_t word0, uint32_t word1) {
+    // printf("alu_mac\\n");
+#if MODE == MODE_EMUL
+    acc += current_weights.word0 * word0;
+    acc += current_weights.word1 * word1;
+#elif MODE == MODE_CFU
+#ifdef SEAL5
+    int32_t acc = __builtin_riscv_xcfu_cfu0_alu_mac(word0, word1);
+#else
+    cfu_op0_hw(CFU_OPCODE_ALU_MAC, word0, word1);
+#endif  // SEAL5
+#endif  // MODE
+    // TODO: if non-zero?
+    return acc;
+}
+
+static void __attribute__((always_inline)) inline alu_rst() {
+    // printf("alu_rst\\n");
+#if MODE == MODE_EMUL
+    acc = 0;
+#elif MODE == MODE_CFU
+#ifdef SEAL5
+    __builtin_riscv_xcfu_cfu0_alu_rst();
+#else
+    cfu_op0_hw(CFU_OPCODE_ALU_RST, 0, 0);
+#endif  // SEAL5
+#endif  // MODE
+}
+
+static int32_t __attribute__((always_inline)) inline get_acc() {
+    // printf("get_acc\\n");
+#if MODE == MODE_EMUL
+    return acc;
+#elif MODE == MODE_CFU
+#ifdef SEAL5
+    return __builtin_riscv_xcfu_cfu0_alu_mac(0, 0);
+#else
+    return cfu_op0_hw(CFU_OPCODE_ALU_MAC, 0, 0);  // TODO: opcode for load?
+#endif  // SEAL5
+#endif  // MODE
+}
+
+int32_t cfu_kernel_32x(int8_t* data_ptr, int8_t* weights_ptr, int32_t* acc) {
+    uint32_t code_word0 = *((uint32_t*)weights_ptr);
+    uint32_t code_word1 = *((uint32_t*)(weights_ptr + 4));
+    uint32_t* act_words = (uint32_t*)data_ptr;
+
+    alu_rst();
+    // cfu_op0(CFU_FUNCT7_PUSH_WEIGHTS, code_word0, code_word1);
+    push_weights_4b(code_word0, code_word1);
+    for (int i = 0; i < 4; i++) {
+        // cfu_op0(CFU_FUNCT7_ALU_MAC, act_words[2 * i], act_words[2 * i + 1]);
+        alu_mac(act_words[2 * i], act_words[2 * i + 1]);
+    }
+    *acc = get_acc();
+    return 42;
+}
+#endif  // CFU_KERNEL_CODE
+"""
+
+@derived_object
+class ImportCPostprocess(ms.postproc.PyPostproc):
+    """A postproc that always fails."""
+
+    def _initialize_with_tune_context(self, context: ms.TuneContext) -> None:
+        pass
+
+    def apply(self, sch: Schedule) -> bool:
+        # print("apply", sch)
+        # return False
+        # has = has_block(sch, "block")
+        has = has_block(sch, "root")
+        # print("has", has)
+        if has:
+            # block = sch.get_block("block")
+            block = sch.get_block("root")
+            # print("block", block)
+            sch.annotate(block, "foo", "bar")
+            sch.annotate(block, "pragma_import_c", C_CODE)
+        # print("sch", sch)
+        # input(">")
+        return True
+
+    def clone(self) -> "ImportCPostprocess":
+        return ImportCPostprocess()
+
+    def __str__(self) -> str:
+        return "ImportCPostprocess"
+
+
+def get_tuning_config(enable_intrin: bool = False):
+    def _get_sch_rules(intrin: Optional[str] = None):
+        structure_lookup = {
+            AMDGPU_SDOT4_INTRIN: "SSSRRSRS",
+            VRMPY_i8i8i32_INTRIN: "SRSRS",
+            DP4A_S8S8S32_INTRIN: "SR",
+            # ARM_DOT_4x4_i8_NEON_INTRIN: "SR",
+            ARM_DOT_4x4_i8_NEON_INTRIN: "RS",
+        }
+        return [
+            ms.schedule_rule.ApplyCustomRule(),
+            ms.schedule_rule.InlineConstantScalars(),
+            ms.schedule_rule.AutoInline(
+                into_producer=False,
+                into_consumer=True,
+                inline_const_tensor=True,
+                disallow_if_then_else=True,
+                require_injective=True,
+                require_ordered=True,
+                disallow_op=["tir.exp"],
+            ),
+            # ms.schedule_rule.AddRFactor(max_jobs_per_core=1, max_innermost_factor=64),
+            *([ms.schedule_rule.MultiLevelTilingWithIntrin(
+                    intrin,
+                    structure=structure_lookup[intrin],
+                    # tile_binds=["blockIdx.x", "vthread.x", "threadIdx.x"],
+                    # max_innermost_factor=32,
+                    # vector_load_lens=[1, 2, 3, 4],
+                    # reuse_read=ms.schedule_rule.ReuseType(
+                    #     req="must",
+                    #     levels=[4],
+                    #     scope="shared",
+                    # ),
+                    # reuse_write=ms.schedule_rule.ReuseType(
+                    #     req="must",
+                    #     levels=[3],
+                    #     scope="local",
+                    # ),
+                )] if intrin is not None else []),
+            ms.schedule_rule.MultiLevelTiling(
+                structure="SSRSRS",
+                # structure="SSRSRS",
+                tile_binds=None,
+                max_innermost_factor=64,
+                vector_load_lens=None,
+                reuse_read=None,
+                reuse_write=ms.schedule_rule.ReuseType(
+                    req="may",
+                    levels=[1, 2],
+                    scope="global",
+                ),
+            ),
+            ms.schedule_rule.ParallelizeVectorizeUnroll(
+                max_jobs_per_core=-1,  # disable parallelize
+                max_vectorize_extent=-1,  # disable vectorize
+                unroll_max_steps=[0, 2, 4, 8, 16, 32, 64],
+                unroll_explicit=True,
+                # unroll_explicit=False,
+            ),
+            ms.schedule_rule.RandomComputeLocation(),
+        ]
+
+    def _get_postprocs():
+        return [
+            ms.postproc.DisallowDynamicLoop(),
+            ms.postproc.RewriteParallelVectorizeUnroll(),
+            ms.postproc.RewriteReductionBlock(),
+            ms.postproc.RewriteTensorize(),
+            # ms.postproc.RewriteTensorize(vectorize_init_loop=True),
+            *([ImportCPostprocess()] if enable_intrin else []),
+        ]
+
+    def _get_mutator_probs():
+        return {
+            ms.mutator.MutateTileSize(): 0.9,
+            ms.mutator.MutateComputeLocation(): 0.05,
+            ms.mutator.MutateUnroll(): 0.03,
+            # ms.mutator.Parallel(): 0.02,
+        }
+
+    default_intrin = DP4A_S8S8S32_INTRIN
+    intrin = default_intrin if enable_intrin else None
+    sch_rules = _get_sch_rules(intrin)
+    postprocs = _get_postprocs()
+    mutator_probs = _get_mutator_probs()
+    return sch_rules, postprocs, mutator_probs
+
 
 def _schedule_dummy():
 
@@ -90,23 +342,41 @@ def create_relay_module():
     # "llvm",
 ])
 @pytest.mark.parametrize("target", [
-    "c",
+    # "c -num-cores 1",
+    "c -device=arm_cpu -mcpu=cortex-m7 -num-cores=1",
     # "llvm -num-cores 1 -mcpu generic-rv64 -mtriple=riscv64-unknown-elf -mabi lp64d -mattr=+d,+f,+m,+64bit -model=etiss-rv64gc",
     # "llvm -num-cores 1 -mcpu generic-rv64 -mtriple=riscv64-unknown-elf -mabi lp64d -mattr=+d,+f,+m,+64bit -model=etiss-rv64gc -global-isel=1 -global-isel-abort=2 -basic-block-sections=1",
 ])
 @pytest.mark.parametrize("num_trials_per_iter,max_trials_per_task,max_trials_global", [
-    # (0, 0, 0),
-    # (1, 1, 3),
-    (5, 10, 3 * 10),
-    # (5, 50, 3 * 50),
-    # (5, 100, 3 * 100),
-    # (5, 200, 3 * 200),
-    # (5, 400, 3 * 400),
-    # (5, 800, 3 * 800),
-    # (5, 1600, 3 * 800),
+    # (0, 0, 1000000),
+    # (1, 1, 1000000),
+    # (5, 10, 1000000),
+    (5, 20, 1000000),
+    # (5, 50, 1000000),
+    # (5, 100, 1000000),
+    # (5, 200, 1000000),
+    # (5, 400, 1000000),
+    # (5, 800, 1000000),
+    # (5, 1600, 1000000),
+])
+@pytest.mark.parametrize("enable_custom,enable_intrin", [
+    # (False, False),
+    # (True, False),
+    (True, True),
+])
+@pytest.mark.parametrize("module_equality", ["ignore-ndarray"])
+@pytest.mark.parametrize("model", [
+    # "default",
+    # "resnet_clustered",
+    # "resnet_clustered_layer14",
+    "resnet_clustered_layer10",
+])
+@pytest.mark.parametrize("transform_layout", [
+    # False,
+    True,
 ])
 @tvm.testing.requires_micro
-def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials_per_iter, max_trials_per_task, max_trials_global):
+def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials_per_iter, max_trials_per_task, max_trials_global, enable_custom, enable_intrin, module_equality, model, transform_layout):
     print()
     from tvm.contrib.micro.meta_schedule.local_builder_micro import get_local_builder_micro
     from tvm.contrib.micro.meta_schedule.rpc_runner_micro import get_rpc_runner_micro
@@ -146,19 +416,118 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
                 x = str(x)
             x = x.replace(" ", "").replace(",", "").replace("/", "").replace(";", "").replace("=", "-").replace("+", "")
             return x
-        fields = [target, toolchain, alter_op, num_trials_per_iter, max_trials_per_task, max_trials_global, ts, opt_level, *sum(map(list, pass_config.items()), []), *[f"no{x}" for x in disabled_pass]]
+        fields = [target, toolchain, alter_op, num_trials_per_iter, max_trials_per_task, max_trials_global, ts, opt_level, enable_custom, enable_intrin, module_equality, model, *sum(map(list, pass_config.items()), []), *[f"no{x}" for x in disabled_pass]]
         label = "-".join([sanitize(x) for x in fields])
         work_dir_path = base_dir / label
     else:
         work_dir = utils.tempdir()
         work_dir_path = work_dir.path
     print("work_dir_path", work_dir_path)
-    # input("1")
-    mod, params, model_info = create_relay_module()
-    input_name = model_info["in_tensor"]
-    input_shape = model_info["in_shape"]
-    input_dtype = model_info["in_dtype"]
-    data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    # MODEL = "default"
+    # MODEL = "resnet_clustered"
+    # MODEL = "resnet_clustered_layer14"
+    MODEL = "resnet_clustered_layer10"
+
+    if MODEL == "default":
+        # input("1")
+        mod, params, model_info = create_relay_module()
+        input_name = model_info["in_tensor"]
+        input_shape = model_info["in_shape"]
+        input_dtype = model_info["in_dtype"]
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered":
+        model = tvmc.load(
+            str(BASE_DIR / "models/pretrainedResnet_clustered_quant_remap.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 32, 32, 3]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered_layer10":  # conv2d(1x16x16x32, 64x1x1x32, 1x8x8x64)
+        model = tvmc.load(
+            str(BASE_DIR / "models/layers/pretrainedResnet_clustered_quant_remap_packed_layer10.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 16, 16, 32]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered_layer11":  # add
+        model = tvmc.load(
+            str(BASE_DIR / "models/layers/pretrainedResnet_clustered_quant_remap_packed_layer11.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 32, 32, 3]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered_layer12":  # avg pool
+        model = tvmc.load(
+            str(BASE_DIR / "models/layers/pretrainedResnet_clustered_quant_remap_packed_layer12.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 32, 32, 3]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered_layer13":  # rehape
+        model = tvmc.load(
+            str(BASE_DIR / "models/layers/pretrainedResnet_clustered_quant_remap_packed_layer13.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 32, 32, 3]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered_layer14":  # fully connected
+        model = tvmc.load(
+            str(BASE_DIR / "models/layers/pretrainedResnet_clustered_quant_remap_packed_layer14.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 32, 32, 3]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    elif MODEL == "resnet_clustered_layer15":  # softmax
+        model = tvmc.load(
+            str(BASE_DIR / "models/layers/pretrainedResnet_clustered_quant_remap_packed_layer15.tflite")
+        )
+        mod = model.mod
+        params = model.params
+
+        input_shape = [1, 32, 32, 3]
+        input_dtype = "int8"
+        data_sample = np.random.rand(*input_shape).astype(input_dtype)
+    else:
+        assert False, f"Unsupported Model: {MODEL}"
+
+    if transform_layout:
+        with tvm.transform.PassContext(
+            opt_level=opt_level,
+            config=pass_config,
+            disabled_pass=disabled_pass,
+        ):
+            desired_layouts = {"qnn.conv2d": ["NHWC", "HWOI"]}
+            # desired_layouts = {"qnn.conv2d": ["NHWC", "OHWI"]}
+
+            # Convert the layout of the graph where possible.
+            seq = transform.Sequential(
+                [
+                    relay.transform.RemoveUnusedFunctions(),
+                    relay.transform.ConvertLayout(desired_layouts),
+                    relay.transform.FoldConstant(),
+                ]
+            )
+            mod = seq(mod)
+
+    print("model.mod", model.mod)
     link_params = True
 
     runtime = relay.backend.Runtime("crt", {"system-lib": True})
@@ -175,6 +544,17 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
     with ms.Profiler() as profiler:
         if not SKIP_TUNING:
             # print("a1")
+            if enable_custom:
+                sch_rules, postprocs, mutator_probs = get_tuning_config(enable_intrin)
+                space = ms.space_generator.PostOrderApply(
+                    sch_rules=sch_rules,
+                    postprocs=postprocs,
+                    mutator_probs=mutator_probs,
+                )
+                strategy = "evolutionary"
+            else:
+                space = "post-order-apply"
+                strategy = "evolutionary"
             evaluator_config = EvaluatorConfig(
                 number=1,
                 repeat=1,
@@ -193,7 +573,8 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
                         target=target,
                         builder=builder,
                         runner=runner,
-                        strategy="evolutionary",
+                        strategy=strategy,
+                        space=space,
                         # num_trials_per_iter=2,
                         num_trials_per_iter=num_trials_per_iter,
                         # max_trials_per_task=10,
@@ -201,7 +582,7 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
                         # max_trials_global=100,
                         max_trials_global=max_trials_global,
                         work_dir=str(work_dir_path),
-                        module_equality="ignore-ndarray",
+                        module_equality=module_equality,
                         pass_config=MappingProxyType(
                             pass_config,
                             # {
@@ -228,8 +609,7 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
                         **pass_config,
                         "relay.backend.use_meta_schedule": True,
                         "relay.backend.tir_converter": "default",
-                        "relay.backend.use_meta_schedule_dispatch": 2,
-                        # "tir.disable_vectorize": True,
+                        "relay.backend.use_meta_schedule_dispatch": MS_DISPATCH,
                         # "tir.enable_debug": True,
                     }
                 ),
@@ -239,8 +619,8 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
             )
     print(profiler.table())
     import time
-    print("sleeping")
-    time.sleep(10)
+    # print("sleeping")
+    # time.sleep(10)
     non_ms_mod: tvm.runtime.Module = ms.relay_integration.compile_relay(
         None,
         mod=mod,
@@ -249,7 +629,7 @@ def test_micro_tuning_with_meta_schedule(alter_op, toolchain, target, num_trials
         pass_config=MappingProxyType(
             {
                 **pass_config,
-                "relay.backend.use_meta_schedule_dispatch": 2,
+                "relay.backend.use_meta_schedule_dispatch": MS_DISPATCH,
             }
         ),
         disabled_pass=disabled_pass,
