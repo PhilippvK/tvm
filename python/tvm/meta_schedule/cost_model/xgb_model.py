428aae8da042c648cb15a3c2acf8b31edf1e5051
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-08-13 07:35:05 +0200

    python/tvm/meta_schedule/cost_model/xgb_model.py: try to disable normalization

--- python/tvm/meta_schedule/cost_model/xgb_model.py
+++ python/tvm/meta_schedule/cost_model/xgb_model.py
@@ -510,7 +510,8 @@ class XGBModel(PyCostModel):
                     f"{key}: {score:.6f}"
                     for key, score in self._validate(
                         xs=new_features,
-                        ys=group.min_cost / new_mean_costs_np,
+                        # ys=group.min_cost / new_mean_costs_np,
+                        ys=new_mean_costs_np,
                     )
                 ),
             )
@@ -540,7 +541,8 @@ class XGBModel(PyCostModel):
         self._train(
             xs=list(itertools_chain.from_iterable([g.features for g in self.data.values()])),
             ys=np.concatenate(
-                [g.min_cost / g.costs for g in self.data.values()],
+                # [g.min_cost / g.costs for g in self.data.values()],
+                [g.costs for g in self.data.values()],
                 axis=0,
             ),
         )
