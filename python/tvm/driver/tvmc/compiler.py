3c87e9babc2224e548b80085e09a6795e589fee3
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-01-26 15:15:24 +0100

    move tvm extensions to top level repo

--- python/tvm/driver/tvmc/compiler.py
+++ python/tvm/driver/tvmc/compiler.py
@@ -50,8 +50,6 @@ from .transform import generate_transform_args, parse_graph_transform_args, appl
 from .shape_parser import parse_shape_string
 from .workspace_pools import generate_workspace_pools_args, workspace_pools_recombobulate
 
-import tvm.contrib.micro.cfu.wca
-
 # pylint: disable=invalid-name
 logger = logging.getLogger("TVMC")
 
