2af5c8934c8a42d6a79d0205538a3f201c4f4863
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-03-22 12:24:38 +0100

    llvm vlen inference with rvv support

--- src/target/llvm/llvm_instance.h
+++ src/target/llvm/llvm_instance.h
@@ -226,6 +226,11 @@ class LLVMTargetInfo {
    */
   const std::string GetJITEngine() const { return jit_engine_; }
   /*!
+   * \brief Get the TVM & LLVM vector_width
+   * \return number of bits for vector width
+   */
+  const int GetVectorWidth() const { return vector_width_; }
+  /*!
    * \brief Get the LLVM optimization level
    * \return optimization level for this target
    */
@@ -339,6 +344,7 @@ class LLVMTargetInfo {
   llvm::CodeModel::Model code_model_ = llvm::CodeModel::Small;
   std::shared_ptr<llvm::TargetMachine> target_machine_;
   std::string jit_engine_ = "mcjit";
+  int vector_width_{0};
 };
 
 /*!
