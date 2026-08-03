6b7e133c41c2a29986e18f27baed8993159d46c3
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-08-03 10:56:54 +0200

    src/relay/backend/te_compiler.cc: multi dispatch

--- src/relay/backend/te_compiler.cc
+++ src/relay/backend/te_compiler.cc
@@ -551,6 +551,8 @@ TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.use_auto_scheduler", Bool);
 TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.use_meta_schedule", Bool);
 TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.use_meta_schedule_dispatch", Integer);
 TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.tir_converter", String);
+TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.force_op_impl_name", String);
+TVM_REGISTER_PASS_CONFIG_OPTION("relay.backend.force_op_name", String);
 
 TVM_REGISTER_GLOBAL("relay.backend._TECompilerGlobal").set_body_typed([]() {
   return TECompiler::Global();
